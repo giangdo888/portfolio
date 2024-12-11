76798b80fed9d4fb678063b0379facc7217030d7
giangdo888 <giangdo1998@gmail.com>
2024-12-11 18:25:59 +1030

    update profile picture link

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,6 +5,6 @@ export const headerData = {
     fullName: 'Truong Giang Do (Austin)',
     title: "Full Stack Developer",
     desciption:"I'm a dedicated software developer who is passionate about learning new technologies and programming languages. I thrive in both collaborative and independent settings, always aiming to bring strong communication skills and attention to detail to my work. ",
-    image: 'https://raw.githubusercontent.com/giangdo888/portfolio/refs/heads/master/src/assets/png/IMG_9936%20Spare.JPG',
+    image: 'https://raw.githubusercontent.com/giangdo888/portfolio/refs/heads/master/src/assets/png/IMG_9936-square.JPG',
     resumePdf: resume
 }
