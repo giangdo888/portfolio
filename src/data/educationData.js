bdd780b831d6155b0c249d8c0d58a0cd53c9a8ed
giangdo888 <giangdo1998@gmail.com>
2025-02-06 10:42:55 +1030

    reorder education

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,16 +1,16 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'University of Information Technology, Vietnam National University',
-        course: 'Bachelor of Computer Engineer',
-        startYear: '2016',
-        endYear: '2020'
-    },
-    {
-        id: 2,
         institution: 'Flinders University, Australia',
         course: 'Master of Computer Science',
         startYear: '2024',
         endYear: 'Present'
+    },
+    {
+        id: 2,
+        institution: 'University of Information Technology, Vietnam National University',
+        course: 'Bachelor of Computer Engineer',
+        startYear: '2016',
+        endYear: '2020'
     },
 ]
